1362988987a3d82a42c95ce2107e7017a3cae8df
Okko Makkonen <okko.makkonen@gmail.com>
2020-07-28 12:17:01 +0300

    Remove recursion from maze generator

--- maze.py
+++ maze.py
@@ -1,13 +1,14 @@
 """
 Implements a simple Maze generator
 
-TODO: remove recursion from maze generator
 """
 
-from random import sample, randint, choice
-from enum import Enum
-from typing import Tuple, Optional
 import sys
+from random import sample, randint
+from enum import Enum
+from typing import Tuple, Optional, Deque, Iterator
+from collections import deque
+
 import numpy as np
 import imageio
 
@@ -44,17 +45,35 @@ class Maze:
 
         dirs = tuple(dir for dir in Direction)
 
-        def visit(r: int, c: int) -> None:
-            visited[r, c] = True
+        class Frame:
+            def __init__(self, r: int, c: int) -> None:
+                self.r, self.c = r, c
+                self.dirs: Optional[Iterator[Direction]] = None
 
-            for dir in sample(dirs, 4):
-                nr, nc = self.new_point(r, c, dir)
+            def __repr__(self) -> str:
+                return f"<Frame {(self.r, self.c)}>"
+
+        stack: Deque[Frame] = deque()
+        stack.append(Frame(0, start_col))
+
+        # Implements the call stack in the heap
+
+        while stack:
+            frame = stack[-1]
+            visited[frame.r, frame.c] = True
+            if frame.dirs is None:
+                frame.dirs = iter(sample(dirs, 4))
+
+            try:
+                dir = next(frame.dirs)
+                nr, nc = self.new_point(frame.r, frame.c, dir)
 
                 if self.is_valid_point(nr, nc) and not visited[nr, nc]:
-                    self.make_open(r, c, dir)
-                    visit(nr, nc)
+                    self.make_open(frame.r, frame.c, dir)
+                    stack.append(Frame(nr, nc))
 
-        visit(0, start_col)
+            except StopIteration:
+                stack.pop()
 
     def new_point(self, r: int, c: int, dir: Direction) -> Tuple[int, int]:
         """Returns a new point in the specified direction"""
@@ -129,12 +148,22 @@ class Maze:
 
         return out
 
-    def to_imagearray(self, line_width: int = 1, box_width: int = 5) -> np.array:
-        """Returns a string of the maze"""
-        out = 255 * np.ones(
+    def to_imagearray(
+        self,
+        cols: Optional[np.array] = None,
+        line_width: int = 1,
+        box_width: int = 3,
+        col_line: Tuple[int, int, int] = (0, 0, 0),
+        col_box: Tuple[int, int, int] = (255, 255, 255),
+    ) -> np.array:
+        """Returns an image of the maze"""
+        # TODO: implement the colors for the boxes
+
+        out = np.zeros(
             (
                 (line_width + box_width) * self.rows + line_width,
                 (line_width + box_width) * self.cols + line_width,
+                3,
             ),
             dtype=np.uint8,
         )
@@ -144,12 +173,21 @@ class Maze:
             for _ in range(line_width):
                 y = 0
                 for c in range(self.cols):
-                    out[x, y : y + line_width] = 0
+                    out[x, y : y + line_width] = col_line
                     y += line_width
-                    if m.horizontal_walls[r, c]:
-                        out[x, y : y + box_width] = 0
+                    if self.horizontal_walls[r, c]:
+                        out[x, y : y + box_width] = col_line
+                    else:
+                        # 0 <= r <= self.rows and 0 <= c < self.cols
+                        if cols is None:
+                            out[x, y : y + box_width] = col_box
+                        else:
+                            if r != self.rows:
+                                out[x, y : y + box_width] = cols[r, c]
+                            else:
+                                out[x, y : y + box_width] = cols[r - 1, c]
                     y += box_width
-                out[x, y : y + line_width] = 0
+                out[x, y : y + line_width] = col_line
                 y += line_width
                 x += 1
 
@@ -159,11 +197,24 @@ class Maze:
             for _ in range(box_width):
                 y = 0
                 for c in range(self.cols + 1):
-                    if m.vertical_walls[r, c]:
-                        out[x, y : y + line_width] = 0
+                    if self.vertical_walls[r, c]:
+                        out[x, y : y + line_width] = col_line
+                    else:
+                        # 0 <= r < self.rows and 0 <= c <= self.cols
+                        if cols is None:
+                            out[x, y : y + line_width] = col_box
+                        else:
+                            if c != self.rows:
+                                out[x, y : y + line_width] = cols[r, c]
+                            else:
+                                out[x, y : y + line_width] = cols[r, c - 1]
                     y += line_width
                     if c == self.cols:
                         break
+                    # Color in the box
+                    out[x, y : y + box_width] = (
+                        cols[r, c] if cols is not None else col_box
+                    )
                     y += box_width
                 x += 1
 
@@ -174,17 +225,22 @@ if __name__ == "__main__":
 
     """Generates a maze, prints it and saves to an image."""
 
+    import random
+
+    random.seed(0)
+
     if len(sys.argv) == 2:
         rows, cols = int(sys.argv[1]), int(sys.argv[1])
     elif len(sys.argv) == 3:
         rows, cols = int(sys.argv[1]), int(sys.argv[2])
     else:
-        rows, cols = 40, 40
+        rows, cols = 50, 50
 
     start = randint(0, cols - 1)
 
     m = Maze(rows, cols, start)
 
-    print(m)
+    if cols <= 50:
+        print(m)
 
     imageio.imwrite("media/maze.png", m.to_imagearray())
